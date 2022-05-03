38a0986098937a43c83fea0407a77763904a19a5
HenryBcit <hleung70@bcit.ca>
2022-05-03 08:53:11 -0700

    more components

---
+++ exs/ex8/datasets/comps/Card/Switch/index.js
@@ -0,0 +1,11 @@
+import styled, {keyframes} from 'styled-components'
+import {useState, useEffect} from 'react'
+
+export default function Switch({
+  //attributes / props
+}){
+
+  return <div>
+    Switch Stuff
+  </div>
+}
